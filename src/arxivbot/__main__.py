6ac431bdbaab9065b8776946cfdfeb8b4c885a6a
Rodolfo Carobene <rodolfo.carobene@gmail.com>
2024-03-03 19:01:14 +0100

    new setup

--- src/arxivbot/__main__.py
+++ src/arxivbot/__main__.py
@@ -1,220 +1,60 @@
-"""Discord bot to fetch new arXiv papers."""
+"""Retrieve papers."""
 
-import datetime
 import os
-import textwrap
 
-import arxiv
-import discord
-import pytz
-from discord.ext import commands, tasks
 from dotenv import load_dotenv
+
+from .utils import (
+    authors_match,
+    extract_papers_from_email,
+    get_email_body,
+    keywords_match,
+)
 
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = os.getenv("GUILD_ID")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-MAX_NUMBER_OF_RESULTS = 20
-TIME = datetime.time(hour=7, minute=00, tzinfo=pytz.timezone("CET"))
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-today = True  # only fetch for today's articles
-TODAY_ARXIV = {}
+IMAP_SERVER = os.getenv("IMAP_SERVER")
+PORT = int(os.getenv("PORT"))
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
 interests = {
-    "categories": ["quant-ph"],
-    "authors": ["Malnou", "Cancelo"],
-    "keywords": ["TWPA", "RFSoC", "superconducting qubit"],
+    "authors": ["Nicolas Roch"],
+    "keywords": [
+        "superconducting qubit",
+        "TWPA",
+        "parametric amplifier",
+        "qudits",
+        "RFSoC",
+        "fluxonium",
+    ],
 }
 
 
-async def send_max_len(channel, text, width=2000):
-    """Send a message splitting it in lines of max 2000 chars."""
-    lines = textwrap.wrap(text, width, break_long_words=False, replace_whitespace=False)
-    for line in lines:
-        await channel.send(line)
-
-
-@bot.event
-async def on_ready():
-    """Set bot with autofetch."""
-    print("Bot started")
-    await fetch_arxiv()
-    # channel = bot.get_channel(CHANNEL_ID)
-    # await print_arxiv(channel)
-    await bot.add_cog(DailyFetch(bot))
-
-
-class DailyFetch(commands.Cog):
-    """Daily fetch of new articles."""
-
-    def __init__(self, bot) -> None:
-        """Initialize task."""
-        self.bot = bot
-        self.my_task.start()
-
-    @tasks.loop(time=TIME)
-    async def my_task(self) -> None:
-        """Fetch for articles and print them."""
-        await fetch_arxiv()
-        await print_arxiv(self.bot.get_channel(CHANNEL_ID))
-
-
-@bot.command()
-async def fetch(channel):
-    """Fetch arXiv for new articles."""
-    await fetch_arxiv()
-    await print_arxiv(channel)
-
-
-@bot.command()
-async def set_today(channel, arg):
-    """If True fetch papers only from today, otherwise with no limits.
-
-    Parameters
-    -----------
-    arg: str
-        Can be True or False
-    """
-    global today
-    if arg == "True":
-        today = True
-    elif arg == "False":
-        today = False
-
-
-@bot.command()
-async def abstract(channel, num):
-    """Print an abstract of one of the fetched papers.
-
-    Parameters
-    -----------
-    num: int
-        The number of the paper whose abstract you want to print.
-    """
-    return_str = f"""{TODAY_ARXIV[int(num)].title}
-
-    {TODAY_ARXIV[int(num)].summary}
-    """
-    await channel.send(return_str)
-
-
-@bot.command()
-async def query(channel):
-    """Return the registered queries."""
-    await send_max_len(channel, str(interests))
-
-
-@bot.command()
-async def add_queries(channel, cat, *keywords):
-    """Add keywords to query.
-
-    Parameters
-    -----------
-    cat: str
-        The category where to add the keywords (categories, authors, keywords).
-    keywords: list(str)
-        Keywords to add.
-    """
-    if cat not in interests.keys():
-        await channel.send(f"Parameter 1 cannot be {cat}")
-    else:
-        for key in keywords:
-            interests[cat].append(key)
-        return_str = f"Current queries:\n{interests}"
-        await send_max_len(channel, return_str)
-
-
-@bot.command()
-async def remove_queries(channel, *keywords):
-    """Remove parameters from queries.
-
-    parameters
-    -----------
-    keyword: list(str)
-        The keywords to remove.
-    """
-    for key in keywords:
-        for cat, vals in interests.items():
-            interests[cat] = [val for val in vals if val != key]
-    return_str = f"Current queries:\n{interests}"
-    await send_max_len(channel, return_str)
-
-
-@bot.command()
-async def clear_query(channel):
-    """Remove all queries."""
-    for cat in interests:
-        interests[cat] = []
-    return_str = f"Current queries:\n{interests}"
-    await send_max_len(channel, return_str)
-
-
-@bot.command()
-async def max_results(channel, num):
-    """Change max number of results to fetch.
-
-    parameters
-    -----------
-    num: int
-        The maximum number of results to output.
-    """
-    global MAX_NUMBER_OF_RESULTS
-    MAX_NUMBER_OF_RESULTS = int(num)
-    await channel.send(f"Current max number: {MAX_NUMBER_OF_RESULTS}")
-
-
-async def print_arxiv(channel):
-    """Format arXiv dictionary and print it."""
-    datetoday = datetime.datetime.today().strftime("%d/%m/%Y")
-    if len(TODAY_ARXIV) == 0:
-        await channel.send(
-            f"(arXivBot update of the {datetoday})\tNo interesting articles today :-("
-        )
+def main():
+    """Execute main function."""
+    body = get_email_body(IMAP_SERVER, PORT, EMAIL, PASSWORD)
+    if body is None:
+        print("Failed to fetch emails.")
         return
-    return_str = f"(arXivBot update of the {datetoday})\tNew interesting paper published on arXiv today!\n"
-    for i, r in enumerate(TODAY_ARXIV.values()):
-        names = r.authors[0].name.split(" ")
-        first_author = f"{names[0][0]} {names[-1]}"
-        return_str += f"\n{i+1}: {r.title} ({first_author}): {r.pdf_url}"
 
-    await send_max_len(channel, return_str)
+    papers = extract_papers_from_email(body)
 
+    matching_papers = []
+    for paper in papers:
+        authors_match(interests["authors"], paper)
+        keywords_match(interests["keywords"], paper)
+        if len(paper.matching) != 0:
+            matching_papers.append(paper)
 
-async def fetch_arxiv():
-    """Fetch arXiv for new articles matching interests."""
-    global TODAY_ARXIV
-    global today
-    TODAY_ARXIV = {}
-    client = arxiv.Client()
-
-    queries = []
-    queries.append(" OR ".join("ca:" + i for i in interests["categories"]))
-    queries.append(" OR ".join("au:" + i for i in interests["authors"]))
-    queries.append(" OR ".join(interests["keywords"]))
-
-    query = " OR ".join(queries)
-
-    search = arxiv.Search(
-        query=query,
-        max_results=MAX_NUMBER_OF_RESULTS,
-        sort_by=arxiv.SortCriterion.SubmittedDate,
+    print(
+        f"\nOf {len(papers)} new papers, {len(matching_papers)} matched your keywords"
     )
 
-    results = client.results(search)
-
-    return_str = ""
-
-    for i, r in enumerate(results):
-        if today:
-            if r.updated.date() != datetime.datetime.today().date():
-                break
-        return_str += f"\n{i+1}: {r.title} ({r.pdf_url})"
-        TODAY_ARXIV[i + 1] = r
+    for idx, paper in enumerate(matching_papers):
+        print(f"\n{idx+1}. {paper}")
+        print(f"Matches: {paper.matching}")
 
 
-bot.run(TOKEN)
+if __name__ == "__main__":
+    main()
---
+++ src/arxivbot/utils.py
@@ -0,0 +1,146 @@
+"""Utility functions for email fetching."""
+
+import email
+import imaplib
+import re
+from dataclasses import dataclass
+from datetime import date, datetime
+from typing import Optional, Union
+
+import pytz
+
+
+def convert_to_datetime(date_str) -> Union[datetime, None]:
+    """Take email date and convert it to datetime obj."""
+    date_obj = email.utils.parsedate_tz(date_str)
+    if date_obj:
+        timestamp = email.utils.mktime_tz(date_obj)
+        return datetime.fromtimestamp(timestamp, tz=pytz.utc)
+    return None
+
+
+@dataclass
+class Paper:
+    """Paper class."""
+
+    arxiv_id: str
+    title: str
+    authors: list[str]
+    categories: str
+    abstract: str
+    link: str
+    matching: Optional[list[str]] = None
+
+    def __post_init__(self):
+        """Correctly initialize empty matching list."""
+        if self.matching is None:
+            self.matching = []
+
+    def __str__(self):
+        """Return string representation."""
+        names = self.authors[0].split(" ")
+        author_str = f"{names[0][0]} {names[-1]}"
+        if len(self.authors) != 1:
+            author_str += " et al."
+        return f"{self.title}, {author_str} ({self.link})"
+
+
+def extract_papers_from_email(email_body):
+    """Return list of paper objects from email body."""
+    papers = []
+    # Regular expression pattern for extracting paper information
+    pattern = re.compile(
+        r"\\\narXiv:(.*?)\nDate:(.*?)\n\nTitle:(.*?)\nAuthors:(.*?)\nCategories:(.*?)\n(?s:.*?)\\\n\s(.*?)\\\s\(\s(.*?)\s",
+        re.DOTALL,
+    )
+
+    matches = pattern.findall(email_body)
+
+    for match in matches:
+        arxiv_id = match[0].strip()
+        title = re.sub(" +", " ", match[2].strip().replace("\n", " "))
+        authors = match[3].strip().replace("\n", " ")
+        categories = match[4].strip()
+        abstract = re.sub(" +", " ", match[5].strip().replace("\n", " "))
+        link = match[6].strip()
+
+        authors = authors.replace(" and ", ", ")
+        authors = re.sub(" +", " ", authors)
+        authors = authors.split(", ")
+
+        paper = Paper(arxiv_id, title, authors, categories, abstract, link)
+        papers.append(paper)
+
+    return papers
+
+
+def get_email_body(IMAP_SERVER, PORT, EMAIL, PASSWORD):
+    """Connect to email and return body of the correct arxiv email from today."""
+    mail = imaplib.IMAP4_SSL(IMAP_SERVER, PORT)
+    mail.login(EMAIL, PASSWORD)
+
+    mail.select("inbox")
+    status, email_ids = mail.search(None, '(SUBJECT "quant-ph daily")')
+
+    if status == "OK":
+        email_id_list = email_ids[0].split()
+        if email_id_list:
+            latest_email_id = email_id_list[-1]
+            status, email_data = mail.fetch(latest_email_id, "(RFC822)")
+            if status == "OK":
+                raw_email = email_data[0][1]
+                msg = email.message_from_bytes(raw_email)
+                body = msg.get_payload(decode=True).decode()
+                # Get today's date
+                today = date.today()
+                received_date_str = msg.get("Date")
+                received_date = convert_to_datetime(received_date_str).date()
+
+                body = re.sub(" +", " ", body)
+                body = re.sub("\r", "", body)
+                # Compare received date with today's date
+                if received_date == today:
+                    print("The email was received today.")
+                    mail.logout()
+                    return body
+                else:
+                    print("The email was not received today.")
+                    mail.logout()
+                    return body
+                    # return None
+    mail.logout()
+
+
+def authors_match(authors_of_interest, paper):
+    """Check if authors of interest are present in a specific paper.
+
+    If they are, add it to the matching parameter of the paper object.
+    """
+    papers_authors = paper.authors
+    lower_of_interest = [name.lower() for name in authors_of_interest]
+    surnames_of_interest = [name.split(" ")[-1] for name in authors_of_interest]
+
+    lower_paper = [name.lower() for name in papers_authors]
+    surnames_paper = [name.split(" ")[-1] for name in papers_authors]
+
+    for idx, author in enumerate(lower_of_interest):
+        if author in lower_paper or surnames_of_interest[idx] in surnames_paper:
+            # only surname matches
+            paper.matching.append(authors_of_interest[idx])
+
+
+def keywords_match(keywords_of_interest, paper):
+    """Check if keywords of interest are present in a specific paper.
+
+    If they are, add it to the matching parameter of the paper object.
+    """
+    title = paper.title
+    abstract = paper.abstract
+
+    lower_keywords = [key.lower() for key in keywords_of_interest]
+    lower_title = title.lower()
+    lower_abstract = abstract.lower()
+
+    for idx, key in enumerate(lower_keywords):
+        if key in lower_title or key in lower_abstract:
+            paper.matching.append(keywords_of_interest[idx])
